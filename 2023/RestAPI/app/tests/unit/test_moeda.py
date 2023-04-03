2e70359c91f3c8520f9addb1d99768dc5330f168
Luciana Wakabayashi <lucianawakabayashi@gmail.com>
2023-04-03 16:53:02 -0300

    path in trouble

--- 2023/RestAPI/app/tests/unit/test_moeda.py
+++ 2023/RestAPI/app/tests/unit/test_moeda.py
@@ -1,20 +1,13 @@
 from unittest import TestCase
-import sys
-import os
-
-path = os.getcwd()
-
-sys.path.insert(0, path)
 
 from models.Conversor import Conversor
 
-
 class TestMoeda(TestCase):
-        
+    
+
     def test_conversor_real_euro(self):
         conversor = Conversor()
         conversor.converte("Real", "Euro", 10)
         expected = {"valor":55.20, "simbolo":"€"}
         self.assertEqual(expected, conversor.converte("Real", "Euro", 10))
         
-    
