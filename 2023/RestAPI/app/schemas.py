dc1c254213a89d0538dbff25fc07387cb790b045
Luciana Wakabayashi <lucianawakabayashi@gmail.com>
2023-04-06 15:15:35 -0300

    User jwt authentication

--- 2023/RestAPI/app/schemas.py
+++ 2023/RestAPI/app/schemas.py
@@ -6,3 +6,17 @@ class ConversorSchema(Schema):
     moeda2 = fields.Str(required=True)
     valor = fields.Str(required=True)
     simbolo = fields.Str(dump_only=True)
+    
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
+
+
+class PlainMoedaShema(Schema):
+    moeda = fields.Str(dump_only=True)
+    simbolo = fields.Str(dump_only=True)
+    valor = fields.Float(dump_only=True)
+
+class MoedaShema(PlainMoedaShema):
+    moedas = fields.List(fields.Nested(PlainMoedaShema(), dump_only=True))
