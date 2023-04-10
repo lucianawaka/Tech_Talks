fe1f2fc61d018d3aae79b2aa95d7c245fad3d558
lucianawaka <lucianawakabayashi@gmail.com>
2023-04-10 11:59:42 -0300

    clean Moeda Class

--- 2023/RestAPI/models/Moeda.py
+++ 2023/RestAPI/models/Moeda.py
@@ -4,12 +4,6 @@ class Moeda:
         self.nome = nome
         self.simbolo = simbolo
         self.valor = valor
-        
-    # def converter_to(self, moeda, valor):
-    #     if moeda.nome == self.nome:
-    #         return valor, self.simbolo
-    #     converted_value = valor * self.valor / moeda.valor
-    #     return round(converted_value, 2), moeda.simbolo
     
     def __str__(self):
         return 'Moeda: {}, Simbolo: {}, Valor: {}'.format(self.nome, self.simbolo, self.valor)
